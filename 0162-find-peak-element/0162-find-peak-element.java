df49a24cf1e950bcf468743faf76360df10899d8
mehulmm2003 <mehulmalviya21@gmail.com>
2026-06-18 15:43:00 +0530

    Time: 0 ms (100%), Space: 44 MB (72.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,41 +1,40 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-       if(nums.length==1){
-        return 0;
-
-       }
-      else if(nums[0]>nums[1]){
-        return 0;
-
-       }
-       else if(nums[nums.length-1]>nums[nums.length-2]){
-        return nums.length-1;
-       }
-       int st=1;
-       int end=nums.length-2;
-       while(st<=end){
-        int mid=(st+end)/2;
-        if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
-            return mid;
+        if (nums.length == 1) {
+            return 0;
         }
-        else if(nums[mid]>nums[mid+1]){
-            end=mid-1;
+        else if(nums[0]>nums[1]){
+            return 0;
 
         }
-        else if(nums[mid]>nums[mid-1]){
-            st=mid+1;
+        else if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
         }
-        else{
-            st=mid+1;
+        int st = 0;
+        int end = nums.length - 1;
+        while (st <= end) {
+            int mid = (st + end) / 2;
+            if (mid == st) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return st;
+                } else {
+                    st = mid + 1;
+                }
+            } else if (mid == end) {
+                if (nums[mid] > nums[mid - 1]) {
+                    return end;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if(nums[mid]>nums[mid-1]) {
+                st = mid + 1;
+            }
+            else{
+                end=mid-1;
+            }
         }
-
-        }
-       return -1;
-
-
-
-
-
-        
+        return -1;
     }
 }

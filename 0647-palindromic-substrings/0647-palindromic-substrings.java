f5179deba87e6649599163ace54335981f2a11a7
mehulmm2003 <mehulmalviya21@gmail.com>
2026-06-19 21:34:58 +0530

    Time: 6 ms (60.41%), Space: 42.7 MB (80.79%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,47 +1,15 @@
 class Solution {
-    int count = 0;
-
     public int countSubstrings(String s) {
-        substringMaker(s);
-        return count;
-
-    }
-
-    public void substringMaker(String s) {
-
-        for (int i = 0; i < s.length(); i++) {
-
-
-            for (int j = i + 1; j <= s.length(); j++) {
-                
-                    String sub = s.substring(i, j);
-                    if (palin(sub)) {
-                        count++;
-                    }
-
-                }
-
-            
+    int count = 0;
+    for (int center = 0; center < 2 * s.length() - 1; center++) {
+        int left  = center / 2;
+        int right = left + center % 2;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
         }
     }
-
-    public boolean palin(String s) {
-        if(s.length()==0){
-            return false;
-        }
-        if (s.length() == 1) {
-            return true;
-        }
-        int st = 0;
-        int end = s.length() - 1;
-        while (st < end) {
-            if (s.charAt(st) != s.charAt(end)) {
-                return false;
-            }
-            st++;
-            end--;
-        }
-        return true;
-    }
-
+    return count;
+}
 }

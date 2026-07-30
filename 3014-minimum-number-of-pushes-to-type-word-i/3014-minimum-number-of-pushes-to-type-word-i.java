529c86691ef11bc5de15ef1dd1440c4a7b101515
mehulmm2003 <mehulmalviya21@gmail.com>
2026-07-30 22:16:41 +0530

    Time: 3 ms (11.24%), Space: 43.6 MB (5.5%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        
+       
+        Arrays.sort(freq);
+     
+        for (int i = 0, j = 25; i < j; i++, j--) {
+            int temp = freq[i];
+            freq[i] = freq[j];
+            freq[j] = temp;
+        }
+        
+        int total = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) break;
+            int cost = (i / 8) + 1; 
+            total += cost * freq[i];
+        }
+        
+        return total;
+    }
+}
